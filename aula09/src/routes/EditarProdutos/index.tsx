ca2d283e841f5da6f9594e9f9079338901c25f3e
leonardobarbosas <leonardobarbosasantos@gmail.com>
2024-09-11 22:53:08 -0300

    update produto

--- aula09/src/routes/EditarProdutos/index.tsx
+++ aula09/src/routes/EditarProdutos/index.tsx
@@ -1,13 +1,72 @@
-import { useParams } from "react-router-dom";
+import { TipoProduto } from "../../types";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function EditarProdutos() {
+  const navigate = useNavigate();
   const { id } = useParams();
+
+  //recuperar o produto da lista de produtos do localStorage
+  const listaDeProdutosString = localStorage.getItem("lista") || "[]";
+  const lista = JSON.parse(listaDeProdutosString);
+
+  const [produto, setProduto] = useState<TipoProduto>({
+    id: 0,
+    nome: "",
+    preco: 0,
+  });
+
+  useEffect(() => {
+    const produtoEncontrado = lista.find(
+      (p: TipoProduto) => p.id === Number(id)
+    );
+    if (produtoEncontrado) {
+      setProduto(produtoEncontrado);
+    }
+  }, []);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (produto) {
+      const idProduto = lista.findIndex(
+        (p: TipoProduto) => p.id === Number(id)
+      );
+      lista.splice(idProduto, 1, produto);
+      localStorage.setItem("lista", JSON.stringify(lista));
+      alert("Produto alterado com sucesso!!");
+      navigate("/produtos");
+    }
+  };
 
   return (
     <div>
       <h1>Editar Produtos</h1>
       <div>
         <p>Parâmetro que veio do componente produto! : {id}</p>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Nome:</label>
+            <input
+              type="text"
+              value={produto.nome}
+              onChange={(e) => setProduto({ ...produto, nome: e.target.value })}
+            />
+          </div>
+          <div>
+            <label>Preço:</label>
+            <input
+              type="number"
+              value={produto.preco}
+              onChange={(e) =>
+                setProduto({ ...produto, preco: Number(e.target.value) })
+              }
+            />
+          </div>
+          <div>
+            <button type="submit">Salvar</button>
+          </div>
+        </form>
       </div>
     </div>
   );
